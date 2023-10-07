565e1fced87e0ada1a7743a72c16f348af0773e7
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 16:06:50 -0500

    Functions

--- LearningJava/main.js
+++ LearningJava/main.js
@@ -1,19 +1,6 @@
-// THERNARY OPERATOR
-const x = 10;
-
-const color = x > 10 ? 'red' : 'blue';
-
-console.log(color)
-
-// SWITCH OPERATOR
-switch(color) {
-    case 'red':
-        console.log('color is red');
-        break;
-    case 'blue':
-        console.log('color is blue');
-        break;
-    default:
-        console.log('color is NOT red or blue');
-        break;
+function addNums(num1 = 1, num2 = 1) {
+    return num1 + num2;
 }
+
+console.log(addNums(2, 2));
+

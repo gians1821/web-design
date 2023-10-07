e92c0479377e37febde6acc05365c6488bb9a9c2
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 17:03:16 -0500

    Manipulating the DOM

--- LearningJava/Crash Cours For Begginers/main.js
+++ LearningJava/Crash Cours For Begginers/main.js
@@ -1,3 +1,11 @@
-const items = document.querySelectorAll('.item');
+const ul = document.querySelector('.items');
 
-items.forEach((item) => console.log(item));
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
+

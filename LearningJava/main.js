6da540cc9ff0e1da6f247fb8b1c5b3d96ab5a372
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 15:30:30 -0500

    Arrays

--- LearningJava/main.js
+++ LearningJava/main.js
@@ -1,6 +1,15 @@
-const s = 'technology, computers, code, it';
+// Arrays - variables that hold multiple values
 
-console.log(s.length);
-console.log(s.toUpperCase());
-console.log(s.substring(0,10));
-console.log(s.split(','));
+const numbers = new Array(1, 2, 3, 4, 5);
+const fruits = ['apples', 'oranges', 'pears', 10, true];
+
+fruits[3] = 'grapes';
+
+fruits.push('mangos'); // Añade una variable al final del array
+fruits.unshift('strawberries'); // Añade una variable al inicio del array
+fruits.pop(); // Elimina la última variable del array
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(Array.isArray('hello'));
+console.log(fruits.indexOf('oranges'));

05a6d7999d0ccbee7112602f350ede896b721c9e
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 15:54:09 -0500

    High Order Array Methods

--- LearningJava/main.js
+++ LearningJava/main.js
@@ -16,35 +16,24 @@ const todos = [
     }
 ];
 
-console.log(todos);
-console.log(todos[1].text);
+// forEach, map, filter
+todos.forEach(function(todo) {
+    console.log(todo.text)
+});
 
-// CONVIRTIENDO A JSON
-
-const todoJSON = JSON.stringify(todos);
-console.log(todoJSON);
-
-console.clear();
-
-// For
-for (let i = 0; i < 10; i++) {
-    console.log(`For Loop Number: ${i}`);
-}
-
-// While
-let i = 0;
-while(i < 10) {
-    console.log(`While Loop Number: ${i}`);
-    i++;
-}
-
-console.clear();
 /*
-for (let i = 0; i < todos.length; i++) {
-    console.log(todos[i].text)
-}
+const todoText = todos.map(function(todo) {
+    return todo.text
+});
 */
-for (let todo of todos) {
-    console.log(todo.text)    
-}
+
+// console.log(todoText);
+
+const todoCompleted = todos.filter(function(todo) {
+    return todo.isCompleted === true;
+}).map(function(todo) {
+    return todo.text;
+})
+
+console.log(todoCompleted);
 

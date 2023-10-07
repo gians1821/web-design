9183ba361dd5d98b0d76a1d949aef6ba7658060f
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 16:38:16 -0500

    Constructor Functions & Prototypes

--- LearningJava/main.js
+++ LearningJava/main.js
@@ -1,5 +1,21 @@
-const addNums = (num1 = 1, num2 = 1) => num1 + num2;
+// Constructor function
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    this.getFullName = function() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
 
-console.log(addNums(2, 5));
+Person.prototype.getBirthYear = function() { 
+    return this.dob.getFullYear();
+}
 
+// Instantiate object
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-6-1985');
 
+console.log(person1);
+console.log(person2.firstName);
+console.log(person1.getFullName());

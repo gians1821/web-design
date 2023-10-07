5dd2618572b09c452efd0f85f169bcaf9ec1b307
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 16:28:24 -0500

    Arrow Functions

--- LearningJava/main.js
+++ LearningJava/main.js
@@ -1,6 +1,5 @@
-function addNums(num1 = 1, num2 = 1) {
-    return num1 + num2;
-}
+const addNums = (num1 = 1, num2 = 1) => num1 + num2;
 
-console.log(addNums(2, 2));
+console.log(addNums(2, 5));
+
 

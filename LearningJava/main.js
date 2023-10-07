08c6b3a8349965c32bcaeb61cb533cfd66cac88c
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 15:59:24 -0500

    Conditionals

--- LearningJava/main.js
+++ LearningJava/main.js
@@ -1,39 +1,10 @@
-const todos = [
-    {
-        id: 1,
-        text: 'Take out trash',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        text: 'Meeting with boss',
-        isCompleted: true
-    },
-    {
-        id: 3,
-        text: 'Dentist appt',
-        isCompleted: false
-    }
-];
+const x = 6;
+const y = 11;
 
-// forEach, map, filter
-todos.forEach(function(todo) {
-    console.log(todo.text)
-});
+if (x == 10) { // VALUE
+    console.log('x is 10');
+}
 
-/*
-const todoText = todos.map(function(todo) {
-    return todo.text
-});
-*/
-
-// console.log(todoText);
-
-const todoCompleted = todos.filter(function(todo) {
-    return todo.isCompleted === true;
-}).map(function(todo) {
-    return todo.text;
-})
-
-console.log(todoCompleted);
-
+if (x > 5 && y > 10) { // VALUE + DATATYPE ('Recommendation: Always use')
+    console.log('x is more than 5 or y is more than 10');
+}

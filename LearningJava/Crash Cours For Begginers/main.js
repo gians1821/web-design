434deab7fcb89cebe0155ff5f19b57ae3b66b063
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 17:10:13 -0500

    Events

--- LearningJava/Crash Cours For Begginers/main.js
+++ LearningJava/Crash Cours For Begginers/main.js
@@ -1,11 +1,13 @@
-const ul = document.querySelector('.items');
-
-// ul.remove();
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Brad';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
-
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target);
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
+
+
 

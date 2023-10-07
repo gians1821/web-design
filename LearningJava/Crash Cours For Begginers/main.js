fc3a0d397e3f3f57b47f6cfd4e454b7d14eeb5ec
gians1821 <productive.gian.112020@gmail.com>
2023-10-07 16:57:37 -0500

    DOM Selection pt.2

--- LearningJava/Crash Cours For Begginers/main.js
+++ LearningJava/Crash Cours For Begginers/main.js
@@ -1,12 +1,3 @@
-// Single element
-/*
-const form = document.getElementById('my-form');
-console.log(document.querySelector('h1'));
-console.log(document.querySelector('.container'));
-console.log(form)
-*/
+const items = document.querySelectorAll('.item');
 
-// Multiple element
-console.log(document.querySelectorAll('.item'));  // Recommendation: Use this
-console.log(document.getElementsByClassName('item'));
-console.log(document.getElementsByTagName('li'));
+items.forEach((item) => console.log(item));

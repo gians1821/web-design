c40b07c0ad81cf436b796cfd71558c59cc9d82ab
gians <gian.samana.1801@gmail.com>
2023-11-01 13:35:26 -0500

    Añadido header responsive

--- Proyecto Final/src/index.js
+++ Proyecto Final/src/index.js
@@ -1,3 +1,4 @@
-const handleClick = () => {
-    document.documentElement.classList.toggle('dark')
+const toggleMenu = () => {
+    document.querySelector("#menu").classList.toggle('top-28')
+    document.querySelector("#menu").classList.toggle('-top-full')
 }

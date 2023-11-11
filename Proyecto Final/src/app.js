193d1275bae30597283324ff82920a44f8a7fa69
gians <gian.samana.1801@gmail.com>
2023-11-11 18:30:55 -0500

    Implementado modo oscuro y botón whatsapp

--- Proyecto Final/src/app.js
+++ Proyecto Final/src/app.js
@@ -2,3 +2,7 @@ const toggleMenu = () => {
     document.querySelector("#menu").classList.toggle('right-0')
     document.querySelector("#menu").classList.toggle('-right-full')
 }
+
+const toggleDark = () => {
+    document.querySelector("html").classList.toggle('dark')
+}

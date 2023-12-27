78bc0f49557704d7e1bf5e09cd9a548aabf0a7c1
gians1821 <gian.samana.1801@gmail.com>
2023-12-27 00:53:45 -0500

    Updated mobile version

--- Semana 05/examen-01/index.js
+++ Semana 05/examen-01/index.js
@@ -3,8 +3,12 @@ const $menu = document.querySelector('#menuHamburguer')
 const $links = document.querySelectorAll('#link')
 const $body = document.querySelector('body')
 const $btnDark = document.querySelector('#btn-dark') 
+const $btnDarkm = document.querySelector('#btn-darkm') 
 const $btnToPEN = document.querySelector('#btn-toPEN')
 const $btnToUSA = document.querySelector('#btn-toUSA')
+const $btnToPENm = document.querySelector('#btn-toPENm')
+const $btnToUSAm = document.querySelector('#btn-toUSAm')
+
 
 $menu.addEventListener('click', () => {
     $navbar.classList.toggle('left-0')
@@ -20,12 +24,23 @@ $btnDark.addEventListener('click', () => {
   $body.classList.toggle('dark')
 })
 
-if ($btnToPEN) {
+$btnDarkm.addEventListener('click', () => {
+  $body.classList.toggle('dark')
+})
+
+if ($btnToPEN || $btnToPENm) {
     $btnToPEN.addEventListener('click', () => {
+        location.href = "../index.html"
+    })
+    $btnToPENm.addEventListener('click', () => {
         location.href = "../index.html"
     })   
 }
 
 $btnToUSA.addEventListener('click', () => {
+    location.href = "en/index.html"
+})
+
+$btnToUSAm.addEventListener('click', () => {
     location.href = "en/index.html"
 })

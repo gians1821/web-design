f1d6e83880c90f9e21fc24b8e8d1043dbc73a37a
gians1821 <gian.samana.1801@gmail.com>
2023-12-27 00:26:10 -0500

    Added whatsapp button and dark mode

--- Semana 05/examen-01/index.js
+++ Semana 05/examen-01/index.js
@@ -1,28 +1,20 @@
 const $navbar = document.querySelector('#navbar')
 const $menu = document.querySelector('#menuHamburguer')
 const $links = document.querySelectorAll('#link')
+const $body = document.querySelector('body')
+const $btnDark = document.querySelector('#btn-dark') 
 
 $menu.addEventListener('click', () => {
     $navbar.classList.toggle('left-0')
 })
 
-$(document).ready(function() {
-  $('a[href^="#"]').click(function() {
-    var destino = $(this.hash);
-    if (destino.length == 0) {
-      destino = $('a[name="' + this.hash.substr(1) + '"]');
-    }
-    if (destino.length == 0) {
-      destino = $('html');
-    }
-    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
-    return false;
-  });
-});
-
 $links.forEach((link) => {
     link.addEventListener('click', () => {
         $navbar.classList.toggle('left-0')
     })
+})
+
+$btnDark.addEventListener('click', () => {
+  $body.classList.toggle('dark')
 })
 
